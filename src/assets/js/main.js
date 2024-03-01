3f2cd8c14b454476e387ce165840f8f7b01a16af
tndls <Kang su in>
2024-03-01 16:39:07 +0900

    레이아웃 완성

---
+++ src/assets/js/main.js
@@ -0,0 +1,11 @@
+import { menu } from "./menu.js";
+import { portfolio } from "./portfolio.js";
+import { link } from "./link.js";
+import { smooth } from "./smooth.js";
+
+window.addEventListener("load", function(){
+    menu();
+    portfolio();
+    link();
+    smooth();
+});
