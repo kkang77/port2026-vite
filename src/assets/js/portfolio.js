2080c5f002ba05f9b1e503e072025a74695ca322
tndls <Kang su in>
2024-03-03 18:33:39 +0900

    포트폴리오 완료

--- src/assets/js/portfolio.js
+++ src/assets/js/portfolio.js
@@ -1,3 +1,25 @@
-export function portfolio() {
-    console.log(portfolio)
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+export function portfolio(){
+    gsap.registerPlugin(ScrollTrigger);
+    const horSection = gsap.utils.toArray(".portfolio__item");
+    
+    gsap.to(horSection, {
+        xPercent: -120 * (horSection.length -1),
+        ease:"none",
+        scrollTrigger: {
+            trigger: "#portfolio",
+            start: "top 100px",
+            end: "+=3000",
+            pin: true,
+            scrub: 1,
+            markers: false,
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+
+            
+        }
+    });
+
 }
