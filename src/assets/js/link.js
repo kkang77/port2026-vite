9f3044aa1f5021b3de1683aa06ba8e5d134d8fa7
tndls <Kang su in>
2024-03-06 17:59:29 +0900

    마무리 완료

--- src/assets/js/link.js
+++ src/assets/js/link.js
@@ -1,3 +1,16 @@
+import { smooth } from "./smooth";
+
 export function link() {
-    console.log(link)
+    document.querySelectorAll("a[href^='#']").forEach((anchor) => {
+        anchor.addEventListener("click", function(e){
+            e.prventDefault();
+
+            const targetId = this.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+
+            if(targetElement){
+                targetElement.scrollIntoView({ behavior: "smooth"} );
+            }
+        });
+    });
 }
